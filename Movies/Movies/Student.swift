ac66b59af6d3040ec421418e1416c717aa2a38b6
Aida Moldaly <Aida.Moldaly@Administrators-iMac-6.local>
2022-05-18 18:04:55 +0600

    1

---
+++ Movies/Movies/Student.swift
@@ -0,0 +1,8 @@
+//
+//  Student.swift
+//  Movies
+//
+//  Created by Aida Moldaly on 18.05.2022.
+//
+
+import Foundation
---
+++ Movies/Movies/StudentCell.swift
@@ -0,0 +1,23 @@
+//
+//  StudentCell.swift
+//  Movies
+//
+//  Created by Aida Moldaly on 18.05.2022.
+//
+
+import UIKit
+
+class StudentCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
